1091d88a0403e64d0395a963ea0cd1438ba59d1c
gianfelix <gfelixramadan@gmail.com>
2023-06-26 14:25:46 +0700

    minpro

--- src/components/content/ArticleList.jsx
+++ src/components/content/ArticleList.jsx
@@ -14,39 +14,39 @@ import {
   ButtonGroup,
   useToast,
 } from "@chakra-ui/react";
-import {BsBookmarksFill} from "react-icons/bs";
+import { BsBookmarksFill } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { addToBookmark } from "../../redux/reducer/ArticlesReducer";
 import axios from "axios";
 
 export const ArticleList = () => {
-const toast = useToast();
-function toastAddBookmark() {
-  toast({
-    title: "Bookmark Article Success",
-    status: "success",
-    duration: 3000,
-    isClosable: true,
-  });
-}
-function toastFailedBookmark() {
-  toast({
-    title: "Bookmark Article Failed",
-    description: "You must login first",
-    status: "error",
-    duration: 3000,
-    isClosable: true,
-  });
-}
+  const toast = useToast();
+  function toastAddBookmark() {
+    toast({
+      title: "Bookmark Article Success",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  }
+  function toastFailedBookmark() {
+    toast({
+      title: "Bookmark Article Failed",
+      description: "You must login first",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
+  }
 
-const login = useSelector((state) => state.AuthReducer.login);
-const dispatch = useDispatch();
+  const login = useSelector((state) => state.AuthReducer.login);
+  const dispatch = useDispatch();
 
   const [articles, setArticles] = useState([]);
   async function getArticles() {
     try {
       const responArticles = await axios.get(
-        "https://minpro-blog.purwadhikabootcamp.com/api/blog"
+        "https://minpro-blog.purwadhikabootcamp.com/api/blog?sort=ASC&page=1"
       );
       setArticles(responArticles.data.result);
     } catch (err) {
@@ -56,14 +56,28 @@ const dispatch = useDispatch();
   getArticles();
   return (
     <>
-      <Box ml={'35px'} mb={"50px"} >
-        <Text fontSize={"40px"} color={"#FFFFFF"} fontWeight={"bold"} fontFamily={"bauhaus"} mb={"10px"}>All Articles</Text>
-        
+      <Box ml={"35px"} mb={"50px"}>
+        <Text
+          fontSize={"40px"}
+          color={"#FFFFFF"}
+          fontWeight={"bold"}
+          fontFamily={"bauhaus"}
+          mb={"10px"}
+        >
+          All Articles
+        </Text>
+
         <Flex wrap={"wrap"} gap={"20px"}>
           {articles.map((item) => {
             return (
-              <Card maxW={"340px"} minW={"340px"} maxH={"500px"} minH={"400px"} borderRadius={"20px"} >
-                <CardBody justify="center" >
+              <Card
+                maxW={"340px"}
+                minW={"340px"}
+                maxH={"500px"}
+                minH={"400px"}
+                borderRadius={"20px"}
+              >
+                <CardBody justify="center">
                   <Center>
                     <Image
                       src={`https://minpro-blog.purwadhikabootcamp.com/${item.imageURL}`}
@@ -73,26 +87,33 @@ const dispatch = useDispatch();
                       alignItems={"center"}
                     />
                   </Center>
-                  <Stack maxW="350px" maxH={"auto"} >
-                    <Text>{item.title}</Text>
-                    <Text>{item.content}</Text>
+                  <Stack maxW="350px" maxH={"auto"}>
+                    <Text fontWeight={"bold"}>{item.title}</Text>
+                    <Text noOfLines={3}>{item.content}</Text>
                     
                   </Stack>
                 </CardBody>
                 <Divider />
                 <CardFooter>
+                <Text mt={"7px"} mr={"15px"} color={"teal"} fontWeight={"bold"}>
+                     Category: {item.Category.name}
+                    </Text>
                   <ButtonGroup>
                     <Button>Like</Button>
-                    {!login? (
-                      <Button variant={"unstyled"}
-                      rightIcon={<BsBookmarksFill/>}
-                      onClick={() => toastFailedBookmark()}>
-                      </Button>
+                    {!login ? (
+                      <Button
+                        variant={"unstyled"}
+                        rightIcon={<BsBookmarksFill />}
+                        onClick={() => toastFailedBookmark()}
+                      ></Button>
                     ) : (
-                      <Button variant={"unstyled"} 
-                      rightIcon={<BsBookmarksFill/>}
-                      onClick={() => dispatch(addToBookmark(articles), toastAddBookmark(), )}>
-                      </Button>
+                      <Button
+                        variant={"unstyled"}
+                        rightIcon={<BsBookmarksFill />}
+                        onClick={() =>
+                          dispatch(addToBookmark(articles), toastAddBookmark())
+                        }
+                      ></Button>
                     )}
                   </ButtonGroup>
                 </CardFooter>

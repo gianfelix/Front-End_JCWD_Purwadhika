1d3e22f696dac342cd22c24545dd53825dbf7350
Gian Felix Ramadan <gfelixramadan@gmail.com>
2023-06-28 10:46:31 +0700

    commit pwa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+    return (
+    <>
+    <div className="App" align="center" >
+      {/* <image width={"200px"} height={"200px"} src="/logo192.png" /> */}
+      <p>Create PWA Example</p>
+      <button onClick={increment}>Increment</button>
+      <p>Count: {count}</p>
+      <button onClick={decrement}>Decrement</button>
+      <p>edit baru</p>
     </div>
+    </>
   );
 }
 
